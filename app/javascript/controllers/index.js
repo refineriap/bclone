9695fcc0beaf789caea5b9983e32fb6c16c48efc
larefineriadedavin <refineriap9@gmail.com>
2023-11-29 16:46:17 -0600

    stimulus controller

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -9,35 +9,3 @@ eagerLoadControllersFrom("controllers", application)
 // Lazy load controllers as they appear in the DOM (remember not to preload controllers in import map!)
 // import { lazyLoadControllersFrom } from "@hotwired/stimulus-loading"
 // lazyLoadControllersFrom("controllers", application)
-var dropdownButton = document.getElementById("dropbtn");
-var dropdownContent = document.getElementById("myDropdown");
-
-
-function handleDropdownItemClick(sectionId) {
-  document.querySelector(sectionId).scrollIntoView({ behavior: 'smooth' });
-  dropdownButton.innerText = document.querySelector(sectionId + " h2").innerText;
-  dropdownContent.style.display = 'none';
-}
-
-
-function toggleDropdown() {
-  dropdownContent.style.display = (dropdownContent.style.display === 'block') ? 'none' : 'block';
-}
-
-
-window.onscroll = function () {
-  var sections = document.querySelectorAll('.section');
-  var currentSection = null;
-
-  sections.forEach(function (section) {
-    var rect = section.getBoundingClientRect();
-    if (rect.top <= window.innerHeight / 2 && rect.bottom >= window.innerHeight / 2) {
-      currentSection = section;
-    }
-  });
-
-  if (currentSection) {
-    dropdownButton.innerText = currentSection.querySelector("img").innerText;
-  }
-};
-
---
+++ app/javascript/controllers/navbar_controller.js
@@ -0,0 +1,40 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="navbar"
+export default class extends Controller {
+  connect() {
+    var dropdownButton = document.getElementById("dropbtn");
+    var dropdownContent = document.getElementById("myDropdown");
+
+
+    function handleDropdownItemClick(sectionId) {
+      document.querySelector(sectionId).scrollIntoView({ behavior: 'smooth' });
+      dropdownButton.innerText = document.querySelector(sectionId + " h2").innerText;
+      dropdownContent.style.display = 'none';
+    }
+
+
+    function toggleDropdown() {
+      dropdownContent.style.display = (dropdownContent.style.display === 'block') ? 'none' : 'block';
+    }
+
+
+    window.onscroll = function () {
+      var sections = document.querySelectorAll('.section');
+      var currentSection = null;
+
+      sections.forEach(function (section) {
+        var rect = section.getBoundingClientRect();
+        if (rect.top <= window.innerHeight / 2 && rect.bottom >= window.innerHeight / 2) {
+          currentSection = section;
+        }
+      });
+
+      if (currentSection) {
+        dropdownButton.innerText = currentSection.querySelector("img").innerText;
+      }
+    };
+
+
+  }
+}
