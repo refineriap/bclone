d1f82511dd246874aab1b5d63c194f44db96ddef
larefineriadedavin <refineriap9@gmail.com>
2023-12-05 11:54:01 -0600

    changes

--- app/javascript/controllers/navbar_controller.js
+++ app/javascript/controllers/navbar_controller.js
@@ -2,8 +2,17 @@ import { Controller } from "@hotwired/stimulus"
 
 
 export default class extends Controller {
-  connect() {  
-    
+  connect() {
+    console.log("it is working!!")
+  }
+  getImage() {
+    console.log("getImage method called!");
+    // Your getImage method logic here
+  }
+
+  newImage() {
+    console.log("sapo triplehpta")
   }
 }
+
   
