cc9044745db851cd258b9816082343c53fa0a0c7
larefineriadedavin <refineriap9@gmail.com>
2023-12-05 17:46:02 -0600

    dropdown smooth

--- app/javascript/controllers/navbar_controller.js
+++ app/javascript/controllers/navbar_controller.js
@@ -14,32 +14,67 @@ export default class extends Controller {
   scrollToSection(event) {
     event.preventDefault();
 
+    // Hide the dropdown menu first
+    this.toggleDropdown();
+
     const sectionId = event.currentTarget.getAttribute("href").substring(1); // Extract section ID from href
     const section = document.getElementById(sectionId);
 
     if (section) {
-      // Add a delay to allow time for the scroll to finish before hiding the dropdown
-      setTimeout(() => {
-        // Hide the dropdown menu
-        this.toggleDropdown();
+      // Remove the 'active' class from all links
+      this.element.querySelectorAll("a").forEach((link) => {
+        link.classList.remove("active");
+      });
 
-        // Remove the 'active' class from all links
-        this.element.querySelectorAll("a").forEach((link) => {
-          link.classList.remove("active");
-        });
+      // Add the 'active' class to the clicked link
+      event.currentTarget.classList.add("active");
 
-        // Add the 'active' class to the clicked link
-        event.currentTarget.classList.add("active");
-      }, 500); // You may need to adjust the delay based on your scroll duration
+      // Scroll to the section
+      section.scrollIntoView({
+        behavior: "smooth",
+        block: "center",
+        inline: "nearest",
+      });
     }
-    
-    // Scroll to the section
-    section.scrollIntoView({
-      behavior: "smooth",
-      block: "center",
-      inline: "nearest",
-    });
   }
+}
+
+  // static targets = ["section"];
+
+  // toggleDropdown() {
+  //   const dropdown = document.getElementById("myDropdown");
+  //   dropdown.style.display = (dropdown.style.display === "block") ? "none" : "block";
+  // }
+
+  // scrollToSection(event) {
+  //   event.preventDefault();
+
+  //   const sectionId = event.currentTarget.getAttribute("href").substring(1); // Extract section ID from href
+  //   const section = document.getElementById(sectionId);
+
+  //   if (section) {
+  //     // Add a delay to allow time for the scroll to finish before hiding the dropdown
+  //     setTimeout(() => {
+  //       // Hide the dropdown menu
+  //       this.toggleDropdown();
+
+  //       // Remove the 'active' class from all links
+  //       this.element.querySelectorAll("a").forEach((link) => {
+  //         link.classList.remove("active");
+  //       });
+
+  //       // Add the 'active' class to the clicked link
+  //       event.currentTarget.classList.add("active");
+  //     }, 500); // You may need to adjust the delay based on your scroll duration
+  //   }
+    
+  //   // Scroll to the section
+  //   section.scrollIntoView({
+  //     behavior: "smooth",
+  //     block: "center",
+  //     inline: "nearest",
+  //   });
+  // }
   // toggleDropdown(event) {
   //   event.preventDefault();
   //   var dropdown = document.getElementById("myDropdown");
@@ -70,7 +105,7 @@ export default class extends Controller {
   //     event.currentTarget.classList.add("active");
   //   }
   // }
-}
+
 
 
 
