f7985b1c5dc6e874a6235ba4e18cca5d4d5e1e25
larefineriadedavin <refineriap9@gmail.com>
2023-12-06 10:00:11 -0600

    js controller

--- app/javascript/controllers/navbar_controller.js
+++ app/javascript/controllers/navbar_controller.js
@@ -14,27 +14,24 @@ export default class extends Controller {
 
   scrollToSection(event) {
     event.preventDefault();
-
-    // Hide the dropdown menu first
+    
     this.toggleDropdown();
 
-    const sectionId = event.currentTarget.getAttribute("href").substring(1); // Extract section ID from href
+    const sectionId = event.currentTarget.getAttribute("href").substring(1); 
     const section = document.getElementById(sectionId);
 
-    if (section) {
-      // Update button text with the name of the selected section
+      if (section) {
       const sectionName = event.currentTarget.innerText;
       document.getElementById('dropbtn').innerText = sectionName;
 
-      // Remove the 'active' class from all links
       this.element.querySelectorAll("a").forEach((link) => {
         link.classList.remove("active");
       });
 
-      // Add the 'active' class to the clicked link
+      
       event.currentTarget.classList.add("active");
 
-      // Scroll to the section
+      
       section.scrollIntoView({
         behavior: "smooth",
         block: "center",
@@ -43,39 +40,7 @@ export default class extends Controller {
     }
   }
 
-  // static targets = ["section"];
-
-  // toggleDropdown() {
-  //   const dropdown = document.getElementById("myDropdown");
-  //   dropdown.style.display = (dropdown.style.display === "block") ? "none" : "block";
-  // }
-
-  // scrollToSection(event) {
-  //   event.preventDefault();
-
-  //   // Hide the dropdown menu first
-  //   this.toggleDropdown();
-
-  //   const sectionId = event.currentTarget.getAttribute("href").substring(1); // Extract section ID from href
-  //   const section = document.getElementById(sectionId);
-
-  //   if (section) {
-  //     // Remove the 'active' class from all links
-  //     this.element.querySelectorAll("a").forEach((link) => {
-  //       link.classList.remove("active");
-  //     });
-
-  //     // Add the 'active' class to the clicked link
-  //     event.currentTarget.classList.add("active");
-
-  //     // Scroll to the section
-  //     section.scrollIntoView({
-  //       behavior: "smooth",
-  //       block: "center",
-  //       inline: "nearest",
-  //     });
-  //   }
-  // }
+  
 }
 
   
