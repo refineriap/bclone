e4057f34842ee5c40c888d5d474c36896bd3bd95
larefineriadedavin <refineriap9@gmail.com>
2024-01-05 09:03:33 -0600

    infinite controller

---
+++ app/javascript/controllers/infinite_controller.js
@@ -0,0 +1,9 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="infinite"
+export default class extends Controller {
+  connect() {
+    console.log("it is connected!!")
+  }
+
+}
