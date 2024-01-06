855401ce7d42fb78215280ab97fd218127181c13
larefineriadedavin <refineriap9@gmail.com>
2024-01-06 15:37:12 -0600

    arrow

--- app/javascript/controllers/overlay_controller.js
+++ app/javascript/controllers/overlay_controller.js
@@ -2,21 +2,21 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="overlay"
 export default class extends Controller {
-  static targets = ["overlay"];
+  static targets = ["overlay", "arrow"];
 
   connect() {
   }
   showOverlay() {
     this.overlayTarget.style.opacity = 1;
     this.overlayTarget.classList.add("visible");
+    this.arrowTarget.classList.add("hidden"); 
   }
   
   hideOverlay() {
     setTimeout(() => {
       this.overlayTarget.style.opacity = 0;
       this.overlayTarget.classList.remove("visible");
+      this.arrowTarget.classList.remove("hidden");
     }, 3800); 
   }
-  
-
 }    
