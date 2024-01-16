958d18516197a540b3ee61ab2a5e2cf55d83e59a
larefineriadedavin <refineriap9@gmail.com>
2024-01-15 18:50:42 -0600

    poll controller

--- app/javascript/controllers/poll_controller.js
+++ app/javascript/controllers/poll_controller.js
@@ -1,7 +1,30 @@
 import { Controller } from "@hotwired/stimulus"
 
-// Connects to data-controller="poll"
 export default class extends Controller {
-  connect() {
-  }
+    static targets = ["result"];
+
+    connect() {
+    }
+
+    vote(event) {
+        // Get the clicked button from the event target
+        const clickedButton = event.target;
+
+        // Disable all buttons except the clicked one
+        this.element.querySelectorAll('[data-target="poll.button"]').forEach(button => {
+            if (button !== clickedButton) {
+                button.disabled = true;
+                button.classList.add("disabled");
+            }
+        });
+
+        // Update background color for the clicked button
+        clickedButton.classList.add("selected");
+
+        // Update percentage of votes in the corresponding poll div
+        const currentPercentage = parseInt(this.resultTarget.innerText.match(/\d+/)[0]) || 0;
+        const newPercentage = currentPercentage + 1;
+        this.resultTarget.innerText = newPercentage + "% AGREED";
+    }
 }
+
