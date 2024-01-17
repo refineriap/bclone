adbb4b544cdd5c45126446b2aac1cf818e67a73a
larefineriadedavin <refineriap9@gmail.com>
2024-01-16 18:04:32 -0600

    changes poll controller

--- app/javascript/controllers/poll_controller.js
+++ app/javascript/controllers/poll_controller.js
@@ -1,30 +1,59 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-    static targets = ["result"];
+    static targets = ["result", "button", "hellNoMessage", "unDecided"];
 
     connect() {
+        this.loadVotes(); // Load previous votes on page load
     }
 
     vote(event) {
-        // Get the clicked button from the event target
         const clickedButton = event.target;
 
         // Disable all buttons except the clicked one
-        this.element.querySelectorAll('[data-target="poll.button"]').forEach(button => {
+        this.element.querySelectorAll('[data-poll-target="button"]').forEach(button => {
             if (button !== clickedButton) {
                 button.disabled = true;
                 button.classList.add("disabled");
             }
         });
 
-        // Update background color for the clicked button
         clickedButton.classList.add("selected");
 
-        // Update percentage of votes in the corresponding poll div
+        // Update and store the vote count in local storage
         const currentPercentage = parseInt(this.resultTarget.innerText.match(/\d+/)[0]) || 0;
         const newPercentage = currentPercentage + 1;
         this.resultTarget.innerText = newPercentage + "% AGREED";
+        this.storeVotes(newPercentage);
+    }
+
+    storeVotes(voteCount) {
+        // Store the vote count in local storage
+        localStorage.setItem("pollVotes", voteCount);
+    }
+
+    loadVotes() {
+        // Load previous votes from local storage on page load
+        const storedVotes = localStorage.getItem("pollVotes");
+        if (storedVotes !== null) {
+            this.resultTarget.innerText = storedVotes + "% AGREED";
+        }
+    }
+
+    voteHellNo() {
+        
+        this.hellNoMessageTarget.style.display = "block";
+        this.buttonTargets.forEach(button => {
+            button.disabled = true;
+            button.classList.add("disabled");
+        });
+    }
+
+    undecidedMessage() {
+        this.unDecidedTarget.style.display = "block";
+        this.buttonTargets.forEach(button => {
+            button.disabled = true;
+            button.classList.add("disabled");
+        });
     }
 }
-
