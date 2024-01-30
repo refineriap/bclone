96cd2e5f773ac9e05b9e020646b94f4587e3944e
larefineriadedavin <refineriap9@gmail.com>
2024-01-30 17:02:20 -0600

    changes heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -6,7 +6,6 @@ Rails.application.configure do
   # Code is not reloaded between requests.
   config.enable_reloading = false
 
-  config.assets.debug = true
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
