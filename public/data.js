d623dc12b679f1fe967fc56ce1e353e8619e1a82
larefineriadedavin <refineriap9@gmail.com>
2024-02-05 09:23:22 -0600

    changes data

--- public/data.js
+++ public/data.js
@@ -8,7 +8,7 @@ const burgersData = [
     year: '2000',
     image: 'chad-montano.jpg',
     price: 8.99,
-  },
+  },         
   {
     name: 'Double Cheeseburger',
     rating: 7.0,
@@ -17,7 +17,7 @@ const burgersData = [
     year: '2000',
     image: 'chad-montano.jpg',
     price: 8.99,
-  },
+  },         
   {
     name: 'Double Cheeseburger',
     rating: 7.0,
@@ -26,26 +26,7 @@ const burgersData = [
     year: '2000',
     image: 'chad-montano.jpg',
     price: 8.99,
-  },
-  {
-    name: 'Double Cheeseburger',
-    rating: 7.0,
-    location: 'Shake Shack',
-    country: 'USA',
-    year: '2000',
-    image: 'chad-montano.jpg',
-    price: 8.99,
-  },
-  {
-    name: 'Double Cheeseburger',
-    rating: 7.0,
-    location: 'Shake Shack',
-    country: 'USA',
-    year: '2000',
-    image: 'chad-montano.jpg',
-    price: 8.99,
-  },
-         
+  },         
 ]  
   
 export default burgersData;
