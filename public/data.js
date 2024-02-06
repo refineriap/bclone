aa10c921bfc99b76e615f1ae33ef1d176cfa8bfc
larefineriadedavin <refineriap9@gmail.com>
2024-02-06 12:51:16 -0600

    changes

--- public/data.js
+++ public/data.js
@@ -5,7 +5,7 @@ const burgersData = [
         location: 'Chubbie\'s',
         country: 'MEXICO',
         year: '2023',
-        image: 'chubbies.png',
+        image: '',
         price: 12,
     },
     {
