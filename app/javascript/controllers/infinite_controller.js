4e4666e245f61c2cc8302260704fb6e4c2941acb
larefineriadedavin <refineriap9@gmail.com>
2024-02-08 21:16:14 -0600

    infinite controller deleted

--- app/javascript/controllers/infinite_controller.js
+++
@@ -1,9 +0,0 @@
-import { Controller } from "@hotwired/stimulus"
-
-// Connects to data-controller="infinite"
-export default class extends Controller {
-  // connect() {
-  //   console.log("it is connected!!")
-  // }
-
-}
