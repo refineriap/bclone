44ea2863c05d908bfda7d67c6893997fb9783208
larefineriadedavin <refineriap9@gmail.com>
2024-03-14 11:58:48 -0400

    new controller animation

---
+++ app/javascript/controllers/animation_controller.js
@@ -0,0 +1,14 @@
+import { Controller } from "@hotwired/stimulus"
+import anime from "animejs"
+// Connects to data-controller="animation"
+export default class extends Controller {
+  connect() {
+    anime({
+      targets: this.element,
+      translateX: 250,
+      rotate: '1turn',
+      backgroundColor: '#FFF',
+      duration: 800
+    });
+  }
+}
