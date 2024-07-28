3ed45550df01995ba25c3ee7f3241011d6d71bc7
refineriap <refineriap9@gmail.com>
2024-07-28 09:40:15 -0400

    changes

--- app/javascript/controllers/scroller_controller.js
+++ app/javascript/controllers/scroller_controller.js
@@ -3,13 +3,13 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="scroller"
 export default class extends Controller {
   connect() {
-    const divElem = document.querySelectorAll("#scroller");
+    const divElem = document.querySelectorAll(".scroller");
     const svgElem = document.querySelectorAll("#svg");
     const pngElem = document.querySelectorAll("#png");
     const scrollAmount = -400;
 
     window.addEventListener("scroll", (event) => {
-        const elements = document.querySelectorAll("#scroller");
+        const elements = document.querySelectorAll(".scroller");
         for (let i = 0; i < elements.length; i++) {
         const rect = elements[i].getBoundingClientRect();
         var { top } = elements[i].getBoundingClientRect();
